75246a924ac54fe37a0dd6ce39b1b40767246b4c
Michal Middleton <jafa81@gmail.com>
2023-11-15 16:24:01 -0600

    Fix CI

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-MKDOCS_VER=$(grep "mkdocs" requirements.txt | cut -d '=' -f 3)
+MKDOCS_VER=$(grep "mkdocs==" requirements.txt | cut -d '=' -f 3)
 CR_IMAGE_NAME=$1
 CR_IMAGE_TAG="${MKDOCS_VER}-$(date +%Y%m%d)"
 
-docker build . \
+docker build \
     --file Dockerfile \
     --tag ${CR_IMAGE_NAME}:${CR_IMAGE_TAG} \
-    --tag ${CR_IMAGE_NAME}:latest
+    --tag ${CR_IMAGE_NAME}:latest \
+    .
